a0fd8f7c8033c8ddfdc5945abbd29b800ce1f0c3
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-10-27 01:25:05 +0300

    LoginScreen added.

---
+++ screens/LoginScreen.js
@@ -0,0 +1,62 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { KeyboardAvoidingView, StyleSheet, Text, View } from "react-native";
+import { Button, Input, Image } from "react-native-elements";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = () => {
+
+  }
+
+  return (
+    <KeyboardAvoidingView behavior="padding" enabled style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri: "https://upload.wikimedia.org/wikipedia/commons/5/56/Logo_Signal..png",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+
+      <Button containerStyle={styles.button} onPress={signIn} title="Login" />
+      <Button containerStyle={styles.button} type="outline" title="Register" />
+      <View style={{height:100}} />
+    </KeyboardAvoidingView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "white"
+  },
+  inputContainer: {
+    width: 300
+  },
+  button: {
+    marginTop: 10,
+    width: 200,
+  }
+});
