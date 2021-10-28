2dfbfb531166d2fa99120430a89b5513bff1b1a6
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-10-28 21:17:48 +0300

    firebase config updated.

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from "firebase/app"
-import "firebase/auth"
-import "firebase/firestore"
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,16 +15,15 @@ const firebaseConfig = {
   appId: "1:190949424630:web:494e121e3cf8dc3c799bd7"
 };
 
-let app;
+  let app;
 
-if(firebase.getApps().length === 0){
-  // Initialize Firebase
-  app = firebase.initializeApp(firebaseConfig);
-} else {
-  app = firebase.getApp()
-}
+  if(firebase.apps.length === 0){
+    app = firebase.initializeApp(firebaseConfig);
+  } else {
+    app = firebase.app();
+  }
 
-const db = getFirestore();
-const auth = getAuth();
+  const db = app.firestore();
+  const auth = firebase.auth();
 
 export {db, auth}
