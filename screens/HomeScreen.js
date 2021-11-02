590f780dff0acfb642a9f3ebf5c0b35c2298f969
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-11-02 23:54:16 +0300

    HomeScreen header icons added.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,40 @@
 import React, { useLayoutEffect } from "react";
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { Avatar } from "react-native-elements";
 import CustomListItem from "../components/CustomListItem";
-import { auth } from "../firebase"
+import { AntDesign, SimpleLineIcons } from "@expo/vector-icons";
+import { auth } from "../firebase";
 
 const HomeScreen = ({ navigation }) => {
+  const signOutUser = () => {
+    auth.signOut().then(() => navigation.replace("Login"));
+  };
+
+
   const headerLeft = (
     <View style={styles.headerLeft}>
-      <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }}/>
+      <TouchableOpacity onPress={signOutUser} activeOpacity={0.5}>
+        <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
+      </TouchableOpacity>
+    </View>
+  );
+
+  const headerRight = (
+    <View style={styles.headerRight}>
+      <TouchableOpacity activeOpacity={0.5}>
+        <AntDesign name="camerao" size={24} color="black" />
+      </TouchableOpacity>
+
+      <TouchableOpacity activeOpacity={0.5}>
+        <SimpleLineIcons name="pencil" size={24} color="black" />
+      </TouchableOpacity>
     </View>
   );
 
@@ -17,8 +44,9 @@ const HomeScreen = ({ navigation }) => {
       headerStyle: { backgroundColor: "#fff" },
       headerTitleStyle: { color: "black" },
       headerLeft: () => headerLeft,
+      headerRight: () => headerRight,
     });
-  }, []);
+  }, [navigation]);
 
   return (
     <SafeAreaView>
@@ -34,5 +62,11 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   headerLeft: {
     marginLeft: 20,
+  },
+  headerRight: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: 80,
+    marginRight: 20,
   },
 });
