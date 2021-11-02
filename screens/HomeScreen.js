5ed12cf2ae7d4a8f5770760c33fa8650f0183a44
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-11-02 22:14:14 +0300

    HomeScreen added. First ListItem added to HomeScreen.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,38 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useLayoutEffect } from "react";
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Avatar } from "react-native-elements";
+import CustomListItem from "../components/CustomListItem";
+import { auth } from "../firebase"
 
-const HomeScreen = () => {
-  return (
-    <View>
-      <Text>Homepage</Text>
+const HomeScreen = ({ navigation }) => {
+  const headerLeft = (
+    <View style={styles.headerLeft}>
+      <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }}/>
     </View>
-  )
-}
+  );
 
-export default HomeScreen
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Signal",
+      headerStyle: { backgroundColor: "#fff" },
+      headerTitleStyle: { color: "black" },
+      headerLeft: () => headerLeft,
+    });
+  }, []);
 
-const styles = StyleSheet.create({})
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <CustomListItem />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  headerLeft: {
+    marginLeft: 20,
+  },
+});
