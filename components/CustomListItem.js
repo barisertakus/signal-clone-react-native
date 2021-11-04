b8e4429143213c230c70ab816184725b210e0707
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-11-05 02:23:55 +0300

    Empty chatScreen added.

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -4,7 +4,7 @@ import { Avatar, ListItem } from "react-native-elements";
 
 const CustomListItem = ({ id, chatName, enterChat }) => {
   return (
-    <ListItem>
+    <ListItem onPress={() => enterChat(id, chatName)} key={id} bottomDivider>
       <Avatar
         rounded
         source={{
@@ -12,7 +12,9 @@ const CustomListItem = ({ id, chatName, enterChat }) => {
         }}
       />
       <ListItem.Content>
-        <ListItem.Title style={styles.fontListTitle}>Chat text</ListItem.Title>
+        <ListItem.Title style={styles.fontListTitle}>
+          {chatName}
+        </ListItem.Title>
         <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
           Subtitle text. Subtitle text. Subtitle text. Subtitle text.
         </ListItem.Subtitle>
